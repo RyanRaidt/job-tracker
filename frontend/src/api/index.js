b4d47570907d8484e8313c36b9e2b22b65625684
ryanraidt <ryanraidt1026@gmail.com>
2025-04-24 13:08:56 -0500

    Updated CORS config

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default api; 
+export default api;
